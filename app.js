a84488fad80f506fe2ff610fd61b5e9ed4e94671
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2023-05-17 21:13:11 -0300

    Ensinando o JS receber tarefas pelo brownser

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 
-const criarItem = () => {
+const criarItem = (tarefa) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = 
     `
         <input type = "checkbox">
-        <div>Teste de Item 01</div>
+        <div>${tarefa}</div>
         <input type = "button" value = "X">
     `
     document.getElementById('todoList').appendChild(item);
