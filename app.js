1cdb73420fa98824f32a8ecba4d3b07a1d698598
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2023-05-17 21:44:26 -0300

    Apagando campos automáticamente após digitar tarefa

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 
+let banco = [
+    {'tarefa': 'Estudar JS', 'status': ''},
+    {'tarefa': 'Terminar série na Netflix', 'status': 'checked'},
+    {'tarefa': 'Terminar série na Netflix', 'status': 'checked'}
+];
 
-const criarItem = (tarefa) => {
+const criarItem = (tarefa, status) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = 
     `
-        <input type = "checkbox">
+        <input type = "checkbox" ${status}>
         <div>${tarefa}</div>
         <input type = "button" value = "X">
     `
     document.getElementById('todoList').appendChild(item);
+};
+
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+};
+
+const atualizarTela = () => {
+    limparTarefas();
+    banco.forEach(item => criarItem(item.tarefa, item.status));
+};
+
+const inserirItem = (e) => {
+    const tecla = e.key;
+    const texto = e.target.value;
+    if(tecla == 'Enter'){
+        banco.push({'tarefa': texto, 'status': ''})
+        atualizarTela();
+        e.target.value = '';
+    };
 }
+
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+
+atualizarTela();
